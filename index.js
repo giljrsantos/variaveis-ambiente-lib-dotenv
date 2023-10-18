e2c072bd4e1cead084684cd487c8c82516374c92
Gilberto Jr <giljrsantos@gmail.com>
2023-10-18 11:52:41 -0300

    Utilizando variáveis de ambiente em aplicações Node.js com a lib dotenv

---
+++ index.js
@@ -0,0 +1,7 @@
+import 'dotenv/config';
+
+//require('dotenv').config()
+
+console.log(`VALOR ${process.env.VARIAVEL_1}`);
+console.log(`VALOR ${process.env.VARIAVEL_2}`);
+console.log(`VALOR ${process.env.VARIAVEL_3}`);
